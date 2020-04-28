8ec2dd967243d3fd3f034b19405855ec396f0047
mishikaraj <mishikaraj99@gmail.com>
2020-04-28 19:47:13 +0530

    Pages are managed as stack

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 221, 1)
+        canvasColor: Color.fromRGBO(255, 254, 221, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize:20,
+                fontFamily:'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              )
+            ),
       ),
       home: CategoriesScreen(),
     );
