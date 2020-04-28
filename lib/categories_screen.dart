00d46a883cb3275cdbc209b8ab1a24a56c96d457
mishikaraj <mishikaraj99@gmail.com>
2020-04-28 20:23:19 +0530

    Pages are managed as stack

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -2,23 +2,53 @@ import 'package:flutter/material.dart';
 import './category_item.dart';
 import './dummy_data.dart';
 
+// class CategoriesScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title:const Text( 'deliMeal'),),
+//       //backgroundColor: Theme.of(context).accentColor,
+//       body: GridView(
+//         padding: const EdgeInsets.all(25),//const so it never rebuild again
+//         children: DUMMY_CATEGORIES
+//             .map(
+//               (catData) => CategoryItem(
+//                   catData.id,
+//                   catData.title,
+//                   catData.color,
+//                 ),
+//                 )
+//             .toList(),
+//         //designing and look of our grid
+//         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+//           maxCrossAxisExtent: 200,
+//           childAspectRatio: 3 / 2,
+//           crossAxisSpacing: 20,
+//           mainAxisSpacing: 20,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text( 'deliMeal'),),
-      //backgroundColor: Theme.of(context).accentColor,
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
       body: GridView(
-        padding: const EdgeInsets.all(25),//const so it never rebuild again
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
-                  catData.title,
-                  catData.color,
-                ),
-                )
+                    catData.id,
+                    catData.title,
+                    catData.color,
+                  ),
+            )
             .toList(),
-        //designing and look of our grid
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
 import './category_meals_screen.dart';
 
+// class CategoryItem extends StatelessWidget {
+//   final String title;
+//   final String id;
+//   final Color color;
+
+//   CategoryItem(this.id,this.title, this.color);
+//   void selectCategory(BuildContext ctx) {
+//     Navigator.of(ctx).push(
+//       MaterialPageRoute(
+//         builder: (_) {
+//           return CategoryMealsScreen(id,title);
+//         },
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     //return GestureDetector(
+//     return InkWell(
+//       onTap: () => selectCategory(context),
+//       splashColor: Theme.of(context).primaryColor,
+//       borderRadius: BorderRadius.circular(15),
+//       child: Container(
+//         padding: const EdgeInsets.all(15),
+//         child: Text(
+//           title,
+//           style: Theme.of(context).textTheme.title,
+//         ),
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(colors: [
+//             color.withOpacity(0.5), //more transparent
+//             color, //end value as less transparent
+//           ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+//           borderRadius: BorderRadius.circular(15),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
+
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(this.id, this.title);
         },
       ),
     );
@@ -18,7 +62,6 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //return GestureDetector(
     return InkWell(
       onTap: () => selectCategory(context),
       splashColor: Theme.of(context).primaryColor,
@@ -30,10 +73,14 @@ class CategoryItem extends StatelessWidget {
           style: Theme.of(context).textTheme.title,
         ),
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            color.withOpacity(0.5), //more transparent
-            color, //end value as less transparent
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
           borderRadius: BorderRadius.circular(15),
         ),
       ),
